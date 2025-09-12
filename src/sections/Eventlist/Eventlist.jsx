7ff676ae9a6c76c93ae4f2bfefacfa24b572fa05
AbiniMA <165294882+AbiniMA@users.noreply.github.com>
2025-09-12 15:39:32 +0530

    event

--- src/sections/Eventlist/Eventlist.jsx
+++ src/sections/Eventlist/Eventlist.jsx
@@ -112,7 +112,7 @@ const Eventlist = () => {
                     <h4 className='text-xl font-normal'>Events</h4>
                     <div className=' flex items-end md:justify-around flex-wrap p-2 gap-2 '>
                         <div style={{ width: '130px', }}>
-                                          <p style={{ fontSize: "14px", fontWeight: "500" }}>Batch</p>
+                                          <p style={{ fontSize: "14px", fontWeight: "500" }}>Status</p>
 
                             <FormControl
                                 variant="outlined"
